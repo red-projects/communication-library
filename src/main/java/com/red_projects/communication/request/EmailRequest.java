df58e4229f4aed21e7eb9a4822a689ecf443382d
Daniel Rojas <daniel.rojas913@gmail.com>
2020-10-02 22:57:15 -0500

    Initial commit communciation library

---
+++ src/main/java/com/red_projects/communication/request/EmailRequest.java
@@ -0,0 +1,41 @@
+package com.red_projects.communication.request;
+
+public class EmailRequest {
+
+    private String emailAddress;
+    private String username;
+    private String passCode;
+    private String redirectUrl;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassCode() {
+        return passCode;
+    }
+
+    public void setPassCode(String passCode) {
+        this.passCode = passCode;
+    }
+
+    public String getRedirectUrl() {
+        return redirectUrl;
+    }
+
+    public void setRedirectUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+}
